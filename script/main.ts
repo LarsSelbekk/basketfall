f07be831a465c8acf94af26a383351c3d085f804
Lars Mitsem Selbekk <lars@selbekk.com>
2024-09-19 23:00:25 +0200

    rename: web worker -> service worker

--- script/main.ts
+++ script/main.ts
@@ -541,7 +541,7 @@ await init();
 async function tryRegisterWebWorker(): Promise<void> {
   try {
     const serviceWorkerResult = await navigator.serviceWorker.register(
-      `../../web-worker.js`, { scope: `/`, type: "module" });
+      `../../service-worker.js`, { scope: `/`, type: "module" });
     console.log(
       `Service worker ${serviceWorkerResult.installing ? "installing" : serviceWorkerResult.waiting
         ? "waiting"
